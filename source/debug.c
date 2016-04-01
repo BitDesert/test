559c3d04bece6f4ca52ed935099a164db310b36b
Michael Stowe <mstowe@chicago.us.mensa.org>
2016-04-01 16:33:03 -0500

    A little more sane debug routines, not as verbose

--- source/debug.c
+++ source/debug.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int DEBUGLVL = 0;
+#define DBGC_MAX_FIXED 22
+
+int DEBUGLEVEL_CLASS[DBGC_MAX_FIXED + 1] = { 0 , 0 };
 
 int dbgtext(const char *format, ...) 
 {
@@ -9,4 +11,5 @@ int dbgtext(const char *format, ...)
 	va_start(args,format);
 	vprintf(format,args);
 	va_end(args);
+	return 0;
 };
--- source/debug.h
+++ source/debug.h
@@ -1,6 +1,38 @@
 #define MAX_DEBUG_LEVEL 1000
+#define DBGC_MAX_FIXED		22
+
+#if (__GNUC__ >= 3)
+/* the strange !! is to ensure that __builtin_expect() takes either 0 or 1
+ *    as its first argument */
+#ifndef likely
+#define likely(x)   __builtin_expect(!!(x), 1)
+#endif
+#ifndef unlikely
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#endif
+#else
+#ifndef likely
+#define likely(x) (x)
+#endif
+#ifndef unlikely
+#define unlikely(x) (x)
+#endif
+#endif
+
+extern int *DEBUGLEVEL_CLASS;
+
+#define DBGC_CLASS 0
+
+#define CHECK_DEBUGLVL( level ) \
+  ( ((level) <= MAX_DEBUG_LEVEL) && \
+      unlikely(DEBUGLEVEL_CLASS[ DBGC_CLASS ] >= (level)))
 
 #define DEBUG( level, body ) \
-  (void)( ((level) <= MAX_DEBUG_LEVEL) && (dbgtext body) )
+ (void)( ((level) <= MAX_DEBUG_LEVEL) && \
+ unlikely(DEBUGLEVEL_CLASS[ DBGC_CLASS ] >= (level))           \
+  && (dbgtext body) )
+
+#define DEBUGLVL( level ) \
+  ( CHECK_DEBUGLVL(level) )
 
 int dbgtext( const char *, ... );
